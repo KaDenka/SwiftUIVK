6600e26a70161358c28d1253a20d07145928af2a
Denis <recmanus@gmail.com>
2021-10-25 21:10:17 +0300

    Initial_making
    Just create the app

---
+++ SwiftUIVK/SwiftUIVK/SwiftUIVKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVKApp.swift
+//  SwiftUIVK
+//
+//  Created by Denis Kazarin on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
