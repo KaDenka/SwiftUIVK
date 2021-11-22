ff3150ccd519159a22c4f14209b37d7a8003d497
Denis <recmanus@gmail.com>
2021-11-22 10:43:15 +0300

    Lesson7_HomeWork_Stage1
    Making TabBarController

---
+++ SwiftUIVK/SwiftUIVK/UIKitCoordination/TabbarController.swift
@@ -0,0 +1,23 @@
+//
+//  TabbarController.swift
+//  SwiftUIVK
+//
+//  Created by Denis Kazarin on 22.11.2021.
+//
+
+import SwiftUI
+
+struct TabbarController: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UITabBarController {
+        let tabbarController = UITabBarController()
+        
+        
+        return tabbarController
+    }
+    
+    func updateUIViewController(_ uiViewController: UITabBarController, context: Context) {
+        <#code#>
+    }
+
+}
